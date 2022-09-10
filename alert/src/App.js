54cc2e930ae50fbe83ba4a49a1ccf749734f02f3
Irina-Bondarenko <96590953+Irina-Bondarenko@users.noreply.github.com>
2022-09-10 19:30:52 +0300

    init

---
+++ alert/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Alert from "./components/Alert";
+
+function App() {
+  return (
+    <div className="App">
+      <Alert color={"yellow"} text={"What is love?"}/>;
+    </div>
+  );
+}
+
+export default App;
